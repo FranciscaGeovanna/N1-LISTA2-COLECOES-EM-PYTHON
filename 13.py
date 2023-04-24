19d16a145445ea2ef8aa4a85b4dad19a3c4a9e7d
FranciscaGeovanna <115251617+FranciscaGeovanna@users.noreply.github.com>
2023-04-24 15:17:50 -0300

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,4 +1,4 @@
-dic = {}
+dicionario = {}
 
 print("Verifica se existe a chave 'profissão' ")
 
@@ -7,9 +7,9 @@ quant = int(input("\nQuantas chaves e valores deseja informar? "))
 for i in range(quant): 
     chave = input("Digite a {}° chave: ".format(i+1))
     valor = input("Digite o {}° valor: ".format(i+1))
-    dic [chave] = valor
+    dicionario[chave] = valor
     
-if "profissão" in dic:
+if "profissão" in dicionario:
     print("\nProfissão está presente no dicionário!")
 else:
     print("\nProfissão não está no dicionário!")
